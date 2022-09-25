9bf3ecc00d9452c1694ac42a0a70132b648a9d2f
JoePro <git@joeproit.com>
2022-09-25 07:31:30 -0500

    Update exercise2.ps1

--- exercise2.ps1
+++ exercise2.ps1
@@ -1,33 +1,29 @@
-#load text into $text
-#1
-$text = Get-Content .\test.txt -Raw 
+#JoePro 9/25/22
+$regex = '' #empty for looping
+$tempfile = '_test2.txt' #tempfile
+$outputfile = 'test2output.txt' #output
 
-#variable to array
-#2
-$array1 = $text.ToCharArray()
+#read file line by line [1]
+#output to temp file
+Foreach($line in Get-Content .\test2.txt)
+{
+    if($line -match $regex){
+        $caps = ($line.ToUpper()) #ALL CAPS
+        $caps[-1..-$caps.Length] -join '' | Out-File -FilePath $tempfile -Append #reverse and write to file [2]
+    }
+    
+}
 
-#reverse array
-[array]::Reverse($array1)
-#this will have literally reversed $array1
-#without requiring a new array
+#flip the line order in the $tempfile [3]
+$rev = Get-Content $tempfile
+Set-Content -Path $outputfile -Value ($rev[($rev.Length-1)..0])
 
-#ALL CAPS CHARATERS IN $array1
-#3
-$array1.ToUpper()
-
-#Convert $array1 to a string
-[string]$array1
-
-
-
+#clean-up
+Remove-Item $tempfile
 
 <#
-0::Source material:
-
-1:: https://stackoverflow.com/questions/7976646/powershell-store-entire-text-file-contents-in-variable
-
-2:: https://devblogs.microsoft.com/scripting/reverse-strings-with-powershell/
-
-3:: https://devblogs.microsoft.com/scripting/powertip-convert-to-upper-case-with-powershell/
-
+Source
+1:: https://shellgeek.com/read-file-line-by-line-in-powershell/
+2:: https://www.cloudnotes.io/revert-a-string-in-powershell/
+3:: https://www.winhelponline.com/blog/flip-or-reverse-text-file-contents-windows/#powershell
 #>
