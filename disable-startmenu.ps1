1784d35a1404dcb4730229a11dbbe05806a98fea
JoePro <git@joeproit.com>
2023-02-23 11:45:55 -0600

    Delete disable-startmenu.ps1

--- disable-startmenu.ps1
+++
@@ -1,4 +0,0 @@
-If ($User -eq "UserName") {
-  Remove-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Start_TrackProgs" -Force
-  New-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackProgs" -Value 0 -Force
-}
