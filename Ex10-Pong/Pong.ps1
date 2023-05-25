89cce5de55506c7613990ac160d7d08ee5e281eb
JoePro <git@joeproit.com>
2023-05-24 23:31:16 -0500

    Update Pong.ps1

--- Ex10-Pong/Pong.ps1
+++ Ex10-Pong/Pong.ps1
@@ -1,42 +1,69 @@
-$ball = @{'x' = 0; 'y' = 0; 'dx' = 1; 'dy' = 1}
-$paddle = @{'y' = 0}
+Add-Type -TypeDefinition @"
+using System;
+using System.Drawing;
+using System.Windows.Forms;
 
-$consoleWidth = $host.UI.RawUI.WindowSize.Width
-$consoleHeight = $host.UI.RawUI.WindowSize.Height
+public class PongForm : Form
+{
+    private PictureBox ball;
+    private Button leftPaddle, rightPaddle;
+    private Timer timer;
+    private int dx = 5, dy = 5;
 
-while ($true) {
-    # Move the ball
-    $ball.x += $ball.dx
-    $ball.y += $ball.dy
+    public PongForm()
+    {
+        // Setup form
+        this.Size = new Size(800, 400);
+        this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        this.MaximizeBox = false;
 
-    # Bounce off the walls
-    if ($ball.x -lt 0 -or $ball.x -ge $consoleWidth) {
-        $ball.dx *= -1
+        // Initialize ball
+        ball = new PictureBox
+        {
+            Size = new Size(20, 20),
+            Location = new Point(this.ClientSize.Width / 2, this.ClientSize.Height / 2),
+            BackColor = Color.Red
+        };
+        this.Controls.Add(ball);
+
+        // Initialize left paddle
+        leftPaddle = new Button
+        {
+            Size = new Size(20, 80),
+            Location = new Point(0, this.ClientSize.Height / 2)
+        };
+        this.Controls.Add(leftPaddle);
+
+        // Initialize right paddle
+        rightPaddle = new Button
+        {
+            Size = new Size(20, 80),
+            Location = new Point(this.ClientSize.Width - 20, this.ClientSize.Height / 2)
+        };
+        this.Controls.Add(rightPaddle);
+
+        // Initialize timer for game loop
+        timer = new Timer();
+        timer.Interval = 50; // change this for speed of game
+        timer.Tick += Tick;
+        timer.Start();
     }
 
-    # Bounce off the paddle
-    if ($ball.y -ge $consoleHeight - 2) {
-        if ($ball.x -ge $paddle.y - 2 -and $ball.x -le $paddle.y + 2) {
-            $ball.dy *= -1
-        }
-        else {
-            break # End the game if the ball hits the bottom and isn't caught
-        }
+    private void Tick(object sender, EventArgs e)
+    {
+        // Move ball
+        ball.Location = new Point(ball.Location.X + dx, ball.Location.Y + dy);
+
+        // Check collision with walls
+        if (ball.Location.Y <= 0 || ball.Location.Y >= this.ClientSize.Height - ball.Height)
+            dy *= -1;
+
+        // Check collision with paddles
+        if (ball.Bounds.IntersectsWith(leftPaddle.Bounds) || ball.Bounds.IntersectsWith(rightPaddle.Bounds))
+            dx *= -1;
     }
-    elseif ($ball.y -lt 0) {
-        $ball.dy *= -1
-    }
-
-    # Move the paddle to follow the ball
-    $paddle.y = $ball.x
-
-    # Draw everything
-    Clear-Host
-    Write-Host "`n" * $ball.y + (" " * $ball.x + "O") # Ball
-    Write-Host "`n" * ($consoleHeight - $ball.y - 3) # Space between the ball and the paddle
-    Write-Host " " * ($paddle.y - 2) + "=====" # Paddle
-
-    Start-Sleep -Milliseconds 100
 }
+"@ -ReferencedAssemblies System.Windows.Forms,System.Drawing
 
-Write-Host "Game Over!"
+$form = New-Object PongForm
+$form.ShowDialog()
